4972fa05e3847a1e4bf9332bd4c43cf304ed2047
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-05 20:30:01 +0530

    backend initialized in localhost with nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-const express = require('express');
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+});
+
+
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000")
+});
