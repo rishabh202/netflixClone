e891e25bb68809bd804aaa7d28751cf81e9f9825
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-09 21:02:54 +0530

    writing login and other backend logic

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,48 @@
+import { User } from '../models/user.model.js';
+
 export async function signup(req, res){
-    res.send("Signup route");
+   
+    try {
+        const {email, password, username} = req.body;
+        if(!email || !password || !username) {
+            return res.status(400).json({success:false,message:"All fileds are required"})
+        }
+
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({success:false,message:"All fields are reqiured"})
+        }
+
+        if(password.length < 8){
+            return res.status(400).json({success:false, message:"Password must be minimun 8 characters"})
+        }
+
+        const existingUserByEmail = await User.findOne({email:email})
+        if(existingUserByEmail){
+            return res.status(400).json({success:false, message:"Email already exists"})
+        }
+        const existingUserByUsername = await User.findOne({username:username})
+        if(existingUserByUsername){
+            return res.status(400).json({success:false, message:"Username already exists"})
+        }
+
+        const PROFILE_PICS = ['/images/userAvatars/blue.png', "/images/userAvatars/green.png", "/images/userAvatars/red.png"];
+
+        const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+
+        const newUser = new User({
+            email, 
+            password, 
+            username, 
+            image
+        });
+        
+        await newUser.save()
+
+    } catch (error) {
+        console.log("Error in singup controller", error.message)
+        res.status(500).json({success: false, message:"Internal server error"});
+    }
 }
 export async function login(req, res){
     res.send("Login route");
