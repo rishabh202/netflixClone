b5a9b31a570b151cc93b497806fcbf7d16339b39
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-15 13:54:23 +0530

    done the login logic as well

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -66,7 +66,38 @@ export async function signup(req, res) {
     }
 }
 export async function login(req, res) {
-    res.send("Login route");
+    try {
+     const {email, password} = req.body;
+
+     if(!email || !password){
+      return res.status(400).json({success: false, message: "All fields are required"});
+     }
+
+     const user = await User.findOne({email: email})
+     if(!user) {
+        return res.status(404).json({success: false, message: "Invalid Credentials"});
+     }
+     
+
+     const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+     if(!isPasswordCorrect){
+        return res.status(400).json({success: false, message: "Invalid Credentials"});
+     }
+     generateTokenAndSetCookie(user._id, res);
+     res.status(200).json({
+        success: true, 
+        user: {
+            ...user._doc,
+            password: ""
+
+        }
+     })
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({success: false, message: "Internal sever error"});
+    }
 }
 
 export async function logout(req, res) {
