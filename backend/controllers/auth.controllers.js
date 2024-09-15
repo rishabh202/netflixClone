3d02cf500a0bc2beff92bf58b5cd42138ceb938d
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-15 11:54:19 +0530

    Generated cookie by setting up the JWT token

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,68 +1,73 @@
 import { User } from '../models/user.model.js';
 import bcrypt from "bcrypt";
+import { generateTokenAndSetCookie } from '../utils/generateToken.js';
 
 
-export async function signup(req, res){
-   
+export async function signup(req, res) {
+
     try {
-        const {email, password, username} = req.body;
-        if(!email || !password || !username) {
-            return res.status(400).json({success:false,message:"All fileds are required"})
+        const { email, password, username } = req.body;
+        if (!email || !password || !username) {
+            return res.status(400).json({ success: false, message: "All fileds are required" })
         }
 
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if(!emailRegex.test(email)){
-            return res.status(400).json({success:false,message:"Please enter a valid email id"})
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ success: false, message: "Please enter a valid email id" })
         }
 
-        if(password.length < 8){
-            return res.status(400).json({success:false, message:"Password must be minimun 8 characters"})
+        if (password.length < 8) {
+            return res.status(400).json({ success: false, message: "Password must be minimun 8 characters" })
         }
 
-        const existingUserByEmail = await User.findOne({email:email})
-        if(existingUserByEmail){
-            return res.status(400).json({success:false, message:"Email already exists"})
+        const existingUserByEmail = await User.findOne({ email: email })
+        if (existingUserByEmail) {
+            return res.status(400).json({ success: false, message: "Email already exists" })
         }
-        const existingUserByUsername = await User.findOne({username:username})
-        if(existingUserByUsername){
-            return res.status(400).json({success:false, message:"Username already exists"})
+        const existingUserByUsername = await User.findOne({ username: username })
+        if (existingUserByUsername) {
+            return res.status(400).json({ success: false, message: "Username already exists" })
         }
-        
-       let pass = password.toString();
+
+        let pass = password.toString();
 
         const salt = bcrypt.genSaltSync(10);
         const hash = await bcrypt.hash(pass, salt);
-        
+
 
         const PROFILE_PICS = ['/images/userAvatars/blue.png', "/images/userAvatars/green.png", "/images/userAvatars/red.png"];
 
         const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
 
         const newUser = new User({
-            email, 
-            password: hash, 
-            username, 
+            email,
+            password: hash,
+            username,
             image
         });
-        
 
+
+        generateTokenAndSetCookie(newUser._id, res);
         await newUser.save()
-        
-        res.status(201).json({success: true, 
-            user: {
-            ...newUser._doc, 
-            password: "",
 
-        },
-     });
+
+        res.status(201).json({
+            success: true,
+            user: {
+                ...newUser._doc,
+                password: "",
+
+            },
+        });
+
     } catch (error) {
         console.log("Error in singup controller", error.message)
-        res.status(500).json({success: false, message:"Internal server error"});
+        res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
-export async function login(req, res){
+export async function login(req, res) {
     res.send("Login route");
 }
-export async function logout(req, res){
+export async function logout(req, res) {
     res.send("Logout route");
 }
