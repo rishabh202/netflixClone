5bca56c234880ac1e8df1b801bed183ed057e2c6
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-18 11:35:58 +0530

    some changes in api url nd local url

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -1,21 +1,22 @@
-import axios from "axios";
 import { ENV_VARS } from "../config/envVars.js";
+import axios from "axios";
+
 
 export const fetchFromTMDB = async (url) => {
-    const options = {
-       
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer ' + ENV_VARS.TMDB_API_KEY
-        }
-      };
-   const response = await axios.get(url, options)
-   if (response.status !== 200) {
+  const options = {
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + ENV_VARS.TMDB_API_KEY
+    },
+  };
+
+  const response = await axios.get(url, options);
+  if (response.status !== 200) {
     throw new Error("Failed to fetch data" + response.statusText);
     
    }
-   
-   return response.data
+
+   return response.data;
 }
 
   
