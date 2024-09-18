fe1e6a364a1b95b8439fec23dec8534cffab4447
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-09-18 12:20:10 +0530

    error continues with other methods as well, cannot fetch any data from api

--- backend/controllers/movie.controllers.js
+++ backend/controllers/movie.controllers.js
@@ -13,3 +13,18 @@ export async function getTrendingMovie(req, res) {
         res.status(500).json({success: false, message: "Internal Server Error"});
     }
 }
+
+export async function getMovieTrailers(req, res) {
+    const { id } = req.params;
+    try {
+      const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`)
+      res.json({success: true, trailers: data.results});
+    } catch (error) {
+        if(error.message.includes("404")){
+            return res.status(404).send(null);
+        }
+
+        res.status(500).json({success: false, message: "internal server error"});
+
+    }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getTrendingMovie } from "../controllers/movie.controllers.js";
+import { 
+
+    getTrendingMovie, 
+    getMovieTrailers
+    
+ } from "../controllers/movie.controllers.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers);
 
 export default router;
